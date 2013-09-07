885b534d9c82c0d731219025ba17376fdbda32aa
Judahh <jh_esse@hotmail.com>
2013-09-07 04:19:24 -0300

    TCP communication update.Not tested yet.(TODO:TEST!!!)

--- PhoneServer/src/util/WorkerRunnable.java
+++ PhoneServer/src/util/WorkerRunnable.java
@@ -1,55 +1,53 @@
 package util;
-import java.io.DataOutputStream;
+
 import java.io.IOException;
+import java.net.InetAddress;
 import java.net.Socket;
-import java.util.ArrayList;
 import java.util.Vector;
 
 public class WorkerRunnable extends Thread {
 
 	protected Socket clientSocket;
-	private Receive receive;
-	private Vector<String> toSend;
+	private Receiver receiver;
+	private Sender sender;
 
 	public WorkerRunnable(Socket clientSocket) throws IOException {
 		this.clientSocket = clientSocket;
-		this.toSend=new Vector<>();
-		this.receive=new Receive(clientSocket.getInputStream());
+		this.receiver = new Receiver(clientSocket.getInputStream());
+		this.sender = new Sender(clientSocket.getOutputStream());
 	}
 
-	public void send(String string) throws IOException {
-		this.toSend.add(string);
+	public Vector<String> getReceived() {
+		return this.receiver.getReceived();
 	}
 	
-	public void send() throws IOException {
-		while (clientSocket.isConnected() && !this.toSend.isEmpty()) {
-			DataOutputStream output = new DataOutputStream(
-					clientSocket.getOutputStream());
-			output.write((this.toSend.get(0) + '\n').getBytes());
-			output.flush();
-			System.out.println(this.toSend.get(0) + '\n');
-			this.toSend.remove(0);
-		}
-		
+	public void send(String toSend) throws IOException {
+		this.sender.send(toSend);
 	}
-	
+
+	public InetAddress inetAddress() {
+		return this.clientSocket.getInetAddress();
+	}
+
+	public String address() {
+		return this.clientSocket.getInetAddress().toString();
+	}
+
 	public boolean isConnected() {
 		return clientSocket.isConnected();
 	}
-	
+
 	public void close() throws IOException {
 		clientSocket.close();
 	}
 
 	public void run() {
 		try {
-			this.receive.start();
-			while (clientSocket.isConnected()) {
-				//System.out.println("fsad");
-				send();
-				//System.out.println("yoy");
-			}
-			this.receive.stop();
+			this.receiver.start();
+			this.sender.start();
+			while (clientSocket.isConnected());
+			this.sender.stop();
+			this.receiver.stop();
 			close();
 		} catch (IOException e) {
 			e.printStackTrace();

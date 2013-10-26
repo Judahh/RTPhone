68f2661c3374f630ca7adc52067fcfc46b3e8615
Judah Holanda <judahholanda7@gmail.com>
2013-10-26 12:50:25 -0300

    update

--- RTPhoneClient/src/remoteMethodInvocation/util/ChangeContactStatusThread.java
+++ RTPhoneClient/src/remoteMethodInvocation/util/ChangeContactStatusThread.java
@@ -1,6 +1,37 @@
 /*
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
+ * Copyright (C) 2013 Judah Holanda Correia Lima <judahholanda7@gmail.com>.
+ *
+ * All Rights Reserved.
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ * 
+ * 
+ * 
+ * Copyright (C) 2013 Judah Holanda Correia Lima <judahholanda7@gmail.com>.
+ *
+ * Todos os direitos reservados.
+ * Esse programa é um software livre: você pode redistribuí-lo e/ou modificá-lo
+ * dentro dos termos da Licença Pública Geral GNU como publicada pela 
+ * Fundação do Software Livre (FSF), na versão 3 da Licença, ou 
+ * (na sua opinião) qualquer versão.
+ *
+ * Este programa é distribuído na esperança de que possa ser útil, 
+ * mas SEM NENHUMA GARANTIA; sem uma garantia implícita de
+ * ADEQUAÇÃO a qualquer MERCADO ou APLICAÇÃO EM PARTICULAR. Veja a 
+ * Veja a Licença Pública Geral GNU para maiores detalhes.
+ *
+ * Você deve ter recebido uma cópia da Licença Pública Geral GNU 
+ * junto com este programa, se não, veja <http://www.gnu.org/licenses/>.
  */
 package remoteMethodInvocation.util;
 
@@ -10,9 +41,10 @@ import view.MainWindow;
 
 /**
  *
- * @author JH
+ * @subAuthor Name <e-mail>
+ * @author Judah Holanda Correia Lima <judahholanda7@gmail.com>
  */
-public class ChangeContactStatusThread extends RemoteMethodInvocationConnectThread{
+public class ChangeContactStatusThread extends RemoteMethodInvocationConnectThread {
 
    public ChangeContactStatusThread(MainWindow mainWindow, Client client) {
       super(mainWindow, client);
